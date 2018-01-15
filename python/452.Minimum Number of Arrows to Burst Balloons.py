2f3955637486f76bfaf2dd2746b1bac6fb0390d6
Leputa <limengyuan@pku.edu.cn>
2018-01-15 18:39:27 +0800

    贪心

---
+++ python/452.Minimum Number of Arrows to Burst Balloons.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMinArrowShots(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        points=sorted(points,key=lambda x:x[1])
+       	cnt=0
+       	end=0
+       	for i in range(len(points)):
+       		if (points[i][0]>end):
+       			cnt+=1
+       			end=points[i][1]
+       	return cnt
+
+
+
+
+print(Solution().findMinArrowShots([[10,16], [2,8], [1,6], [7,12]]))
