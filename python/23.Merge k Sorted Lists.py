f6cb54aaac3f41b4e41b39c3db794ae472ec2a21
Leputa <limengyuan@pku.edu.cn>
2018-03-05 21:27:29 +0800

    优先级队列,比败者树简单...

---
+++ python/23.Merge k Sorted Lists.py
@@ -0,0 +1,42 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+from queue import PriorityQueue
+
+class Solution:
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        pq=PriorityQueue()
+        head=ListNode(None)     #常见的建立头结点的方法
+        tmp=head
+        for index,listNode in enumerate(lists):
+        	if listNode!=None:
+        		pq.put((listNode.val,index,listNode))
+        while(pq.qsize()>0):
+        	tt=pq.get()
+        	tmpNode,index=tt[2],tt[1]
+        	tmp.next=tmpNode
+        	tmp=tmp.next
+        	tmpNode=tmpNode.next
+        	if tmpNode!=None:
+        		pq.put((tmpNode.val,index,tmpNode))
+        return head.next
+
+
+node1=ListNode(1)
+node1.next=ListNode(2)
+
+node2=ListNode(1)
+node2.next=ListNode(2)
+
+lists=[node1,node2]
+Solution().mergeKLists(lists)
+
+
+
+
