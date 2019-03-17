1ec11996bdc83108fac875b546c6447ddfa19e46
leputa <limengyuan@pku.edu.cn>
2019-03-17 14:44:59 +0800

    水题

--- python/846.Hand of Straights.py
+++ python/846.Hand of Straights.py
@@ -1,4 +1,18 @@
+from collections import Counter
+
 class Solution:
     def isNStraightHand(self, hand: List[int], W: int) -> bool:
         if hand % W != 0:
             return False
+        count = Counter()
+        for h in hand:
+            count[h] += 1
+        hand = sorted(hand)
+        for h in hand:
+            if count[h] == 0:
+                continue
+            for i in range(W):
+                if count[h+i] == 0:
+                    return False
+                count[h+i] -= 1
+        return True
