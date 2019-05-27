c7db83e9ff19ef4bddb086b7dbcee63403aa7e42
leputa <limengyuan@pku.edu.cn>
2019-05-27 22:54:30 +0800

    想不明白，为什么速度慢

---
+++ python/106.Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -0,0 +1,15 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if len(inorder) == 0:
+            return None
+        root = TreeNode(postorder[len(postorder) - 1])
+        idx = inorder.index(postorder[len(postorder) - 1])
+        root.left = self.buildTree(inorder[:idx], postorder[:idx])
+        root.right = self.buildTree(inorder[idx+1:], postorder[idx:-1])
+        return root
