be6f5065e802c572cdc849a93b29f4395b2b7684
leputa <limengyuan@pku.edu.cn>
2019-06-02 15:32:20 +0800

    复习复习

---
+++ cpp/122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int length = prices.size();
+        if (length <= 1)
+            return 0;
+
+        int profit = 0;
+        int min_sells_prices = prices[0];
+        for (int i = 1; i < length; i++){
+            if (prices[i] < min_sells_prices)
+                min_sells_prices = prices[i];
+            else{
+                profit += prices[i] - min_sells_prices;
+                min_sells_prices = prices[i];
+            }
+        }
+
+        return profit;
+    }
+};
+
--- cpp/cmake-build-debug/CMakeFiles/cpp.dir/DependInfo.cmake
+++ cpp/cmake-build-debug/CMakeFiles/cpp.dir/DependInfo.cmake
@@ -5,6 +5,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
   "/Users/limengyuan/Documents/Computer Science/Leetcode/cpp/121_Best_Time_to_Buy_and_Sell_Stock.cpp" "/Users/limengyuan/Documents/Computer Science/Leetcode/cpp/cmake-build-debug/CMakeFiles/cpp.dir/121_Best_Time_to_Buy_and_Sell_Stock.cpp.o"
+  "/Users/limengyuan/Documents/Computer Science/Leetcode/cpp/122_Best_Time_to_Buy_and_Sell_Stock_II.cpp" "/Users/limengyuan/Documents/Computer Science/Leetcode/cpp/cmake-build-debug/CMakeFiles/cpp.dir/122_Best_Time_to_Buy_and_Sell_Stock_II.cpp.o"
   "/Users/limengyuan/Documents/Computer Science/Leetcode/cpp/main.cpp" "/Users/limengyuan/Documents/Computer Science/Leetcode/cpp/cmake-build-debug/CMakeFiles/cpp.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
