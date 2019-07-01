30d8921ec92183438c14abeeaaedba7e4e968892
leputa <limengyuan@pku.edu.cn>
2019-07-01 19:11:01 +0800

    ai

--- cpp/1.Two Sum.cpp
+++ cpp/1.Two Sum.cpp
@@ -10,6 +10,16 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int>ret;
         unordered_map<int, int>map;
-        for(int i=0; i<nums.size(); i++)
+        for(int i=0; i<nums.size(); i++){
+            map[target- nums[i]] = i;
+        }
+        for (int i=0; i<nums.size(); i++){
+            if (map.count(nums[i]) != 0 && map[nums[i]] != i){
+                ret.push_back(i);
+                ret.push_back(map[nums[i]]);
+                break;
+            }
+        }
+        return ret;
     }
 };
