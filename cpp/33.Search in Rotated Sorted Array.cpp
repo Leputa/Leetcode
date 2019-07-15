2ed0414f5073d7a47fbeb7a39c7b5225c3286286
leputa <limengyuan@pku.edu.cn>
2019-07-15 17:46:38 +0800

    gg

--- cpp/33.Search in Rotated Sorted Array.cpp
+++ cpp/33.Search in Rotated Sorted Array.cpp
@@ -10,13 +10,13 @@ public:
             int mid = (low + high) / 2;
             if (nums[mid] == target)
                 return mid;
-            else if (nums[low] < nums[mid]){
+            if (nums[low] <= nums[mid]){
                 if (target < nums[mid] && target >= nums[low])
                     high = mid - 1;
                 else
                     low = mid + 1;
             }
-            else if (nums[mid] < nums[high]){
+            else{
                 if (target > nums[mid] && target <= nums[high])
                     low = mid + 1;
                 else
