386a3ab8e80600339eb91f421dc0ddc9f0405b0a
leputa <limengyuan@pku.edu.cn>
2019-07-17 22:09:06 +0800

    忘了

--- cpp/134.Gas Station.cpp
+++ cpp/134.Gas Station.cpp
@@ -7,5 +7,15 @@ public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         if (accumulate(gas.begin(),gas.end(),0) < accumulate(cost.begin(), cost.end(), 0))
             return -1;
+        int remain = 0;
+        int idx = 0;
+        for (int i=0; i<gas.size(); i++){
+            remain += (gas[i] - cost[i]);
+            if (remain < 0){
+                idx = i+1;
+                remain = 0;
+            }
+        }
+        return idx;
     }
 };
